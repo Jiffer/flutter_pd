e0c1aa600c0662095ee04edb6889c8feeb4dbb4c
yuya.matsuo <yuya.matqqsuo@gree.net>
2020-09-17 00:52:09 +0900

    Simple demo is now working on iOS!!!!!!!

--- ios/Classes/SwiftFlutterPdPlugin.swift
+++ ios/Classes/SwiftFlutterPdPlugin.swift
@@ -1,14 +1,84 @@
 import Flutter
 import UIKit
+import libpd_ios
 
 public class SwiftFlutterPdPlugin: NSObject, FlutterPlugin {
   public static func register(with registrar: FlutterPluginRegistrar) {
-    let channel = FlutterMethodChannel(name: "flutter_pd", binaryMessenger: registrar.messenger())
-    let instance = SwiftFlutterPdPlugin()
+    let channel = FlutterMethodChannel(
+      name: "flutter_pd/method",
+      binaryMessenger: registrar.messenger()
+    )
+    let instance = SwiftFlutterPdPlugin(registrar: registrar)
     registrar.addMethodCallDelegate(instance, channel: channel)
   }
 
+  private let registrar: FlutterPluginRegistrar
+  private var audioController: PdAudioController?
+  private var fileHandles: [Int: UnsafeMutableRawPointer] = [:]
+
+  public init(registrar: FlutterPluginRegistrar) {
+    self.registrar = registrar
+    super.init()
+  }
+
   public func handle(_ call: FlutterMethodCall, result: @escaping FlutterResult) {
-    result("iOS " + UIDevice.current.systemVersion)
+    switch call.method {
+    case "checkPermission":
+      result(true)
+    case "startPd":
+      audioController = PdAudioController()
+      result(nil)
+    case "stopPd":
+      audioController = nil
+      result(nil)
+    case "openAsset":
+      guard let pdFileAssetPath = call.arguments as? String else {
+        result(0)
+        return
+      }
+      let assetPath = registrar.lookupKey(forAsset: pdFileAssetPath)
+      guard let path = Bundle.main.path(forResource: assetPath, ofType: nil) else {
+        result(0)
+        return
+      }
+      let url = URL(fileURLWithPath: path)
+      let fileName = url.lastPathComponent
+      let directory = url.deletingLastPathComponent().path
+      guard let handle = PdBase.openFile(fileName, path: directory) else {
+        result(0)
+        return
+      }
+
+      let hashValue = handle.hashValue
+      fileHandles[hashValue] = handle
+      result(hashValue)
+    case "close":
+      guard let hashValue = call.arguments as? Int, let rawPointer = fileHandles[hashValue] else {
+        result(nil)
+        return
+      }
+      PdBase.closeFile(rawPointer)
+      result(nil)
+    case "startAudio":
+      guard let args = call.arguments as? [String: Any?],
+        let requireInput = args["requireInput"] as? Bool
+      else {
+        result(nil)
+        return
+      }
+      audioController?.configurePlayback(
+        withSampleRate: 44100,
+        numberChannels: 2,
+        inputEnabled: requireInput,
+        mixingEnabled: false
+      )
+      audioController?.isActive = true
+      audioController?.print()
+      result(nil)
+    case "send":
+      result(nil)
+    default:
+      result(FlutterMethodNotImplemented)
+    }
   }
 }
